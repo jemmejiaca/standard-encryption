7e06558dd8aaf7c116ee900858e55b93657204a4
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2016-09-23 13:11:20 -0500

    Implement DES PC1

--- des/des.py
+++ des/des.py
@@ -38,6 +38,18 @@ def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i + n]
+        
+# Print lists with a space every j elements with a message as prefix.
+def print_list(msg, l, j):
+	left_spaces = 11
+	msg_len = len(msg)
+	s = ''
+	c = 0
+	for i in l:
+		if c % j == 0 and c > 0: s = s + ' '
+		s = s + str(i)
+		c = c + 1
+	print msg + (' ' * (left_spaces - msg_len)), s
 
 # Generates a random array of 64 bits
 def generate_random_key():
@@ -48,25 +60,40 @@ def generate_random_key():
 
 # The des encryption algortihm for a single block and 128-length key.
 def des_encrypt(block, key):
+	print "-------------------DES Encryption-------------------"
+	print_list('block =', block, 8)
+	print_list('key =', key, 8)
+	
 	pblock = initial_permutation(block)
-	print "perm_block", pblock
-	print block
+	pkey = create_subkeys(key)
 
 # Makes the initial permutation (IP) on the plaintext block.
 def initial_permutation(block):
-	perm_block = ['e']*64
+	perm_block = ['e'] * 64
 	IP = transforms[PERM][INITIAL]
 	for i in range(64):
-		print i, IP[i] - 1 
+		#print i, IP[i] - 1 
 		perm_block[i] = block[IP[i] - 1]
+	print_list('IP(block) =', perm_block, 8)
 	return perm_block
+	
+def create_subkeys(key):
+	perm_key = ['e'] * 56
+	PC1 = transforms[PERM]["1"]
+	#print "pc1 len", len(PC1)
+	for i in range(56):
+		print i, PC1[i] - 1, key[PC1[i] - 1]
+		perm_key[i] = key[PC1[i] - 1]
+	print_list('PC1(key) =', perm_key, 7)
+	return perm_key
+
 #               -------- Example from the slides --------
 
 my_key = convert_to_bits(1, 4) + convert_to_bits(3, 4) + convert_to_bits(3, 4) + \
 	convert_to_bits(4, 4) + convert_to_bits(5, 4) + convert_to_bits(7, 4) + \
 	convert_to_bits(7, 4) + convert_to_bits(9, 4) + convert_to_bits(9, 4) + \
-	convert_to_bits(12, 4) + convert_to_bits(12, 4) + convert_to_bits(13, 4) + \
-	convert_to_bits(14, 4) + convert_to_bits(15, 4) + convert_to_bits(15, 4) + \
+	convert_to_bits(11, 4) + convert_to_bits(11, 4) + convert_to_bits(12, 4) + \
+	convert_to_bits(13, 4) + convert_to_bits(15, 4) + convert_to_bits(15, 4) + \
 	convert_to_bits(1, 4)
 
 my_block = convert_to_bits(0, 4) + convert_to_bits(1, 4) + convert_to_bits(2, 4) + \
@@ -75,8 +102,6 @@ my_block = convert_to_bits(0, 4) + convert_to_bits(1, 4) + convert_to_bits(2, 4)
 	convert_to_bits(9, 4) + convert_to_bits(10, 4) + convert_to_bits(11, 4) + \
 	convert_to_bits(12, 4) + convert_to_bits(13, 4) + convert_to_bits(14, 4) + \
 	convert_to_bits(15, 4)
-	
-print "my_block =", my_block
-print "my_key =", my_key
+
 des_encrypt(my_block, my_key)
 
