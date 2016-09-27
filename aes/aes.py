3a4510099304f7cbfa4e67de5c3902db96405f8e
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2016-09-27 01:52:08 -0500

    Implement round keys generation

--- aes/aes.py
+++ aes/aes.py
@@ -28,6 +28,23 @@ with open('_maps.json') as data_file:
 	
 sbox = boxes['SBOX']
 
+rcon = [0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a,
+        0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39,
+        0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a,
+        0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8,
+        0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef,
+        0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc,
+        0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b,
+        0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3,
+        0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94,
+        0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20,
+        0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35,
+        0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f,
+        0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04,
+        0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63,
+        0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd,
+        0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb]
+
 def aes128(block, cipher_key):
 	cipher_block = ''
 	state = build_state_matrix(block)
@@ -37,63 +54,104 @@ def aes128(block, cipher_key):
 	
 def generate_subkeys(key):
 	W = [[]] * 44
+	#r = [10 ** ((i-4)/4) for i in xrange(4, 45)]
+	#rcon = [0]*44
+	#print "r", r
 	# Fill W with the four first columns
+	round_key_index = 0
+	round_keys = [[]]*11
+	round_keys[round_key_index] = key
 	W[0] = [key[j][0] for j in xrange(4)]
 	W[1] = [key[j][1] for j in xrange(4)]
 	W[2] = [key[j][2] for j in xrange(4)]
 	W[3] = [key[j][3] for j in xrange(4)]
 	print '--------------------------------- Key Generation -------------------------------------'
-	print ' i | W[i-1] | (1)rot_word() | (2)sub_word() | r_con[i/4] | (3)(1)xor(2) | W[i-4]  W[i]'
+	print ' i | W[i-1] | rot_word | sub_word | rcon[i/4] |   xor   |  W[i-4]  |   W[i]'
 	print '--------------------------------------------------------------------------------------'
 	for i in xrange(4, 44):
 		if i % 4 == 0:
+			if i >= 8:
+				round_keys[round_key_index] = round_key
+			round_key_index += 1
 			rot = rot_word(W[i-1])
-			print '--------------------------------------------------------------------------------------'
-			print '{0:2d}  {1} {2} {3} {4} {5} {6} {7}'.format(i, ''.join(W[i-1]), ''.join(rot), '', '', '', ''.join(W[i-4]), ''.join(W[i]))
+			sub = sub_word(rot)
+			new = hex(int(sub[0], 16) ^ rcon[i/4])[2:]
+			new_sub = [new] + sub[1:]
+			W[i] = xor(W[i - 4], new_sub)
 			
-			W[i] = xor(W[i - 4], T(W[i - 1]))
+			round_key = [W[i]]
+			print '--------------------------------------------------------------------------------------'
+			print '{0:2d}  {1}  {2}   {3:8}      {4:8} {5:8}  {6:8}   {7}'.format(i, ''.join(W[i-1]), ''.join(rot), ''.join(sub), hex(rcon[i/4]), ''.join(new_sub), ''.join(W[i-4]), ''.join(W[i]))
 		else:
-			print '{0:2d}  {1} {2:8} {3:8} {4:8} {5:8} {6} {7}'.format(i, ''.join(W[i-1]), '', '', '', '', ''.join(W[i-4]), ''.join(W[i]))
 			W[i] = xor(W[i - 4], W[i - 1])
+			round_key.append(W[i])
+			print '{0:2d}  {1}  {2:8}   {3:8}      {4:8} {5:8}  {6:8}   {7}'.format(i, ''.join(W[i-1]), '', '', '', '', ''.join(W[i-4]), ''.join(W[i]))
+		round_keys[-1] = round_key
+		
+	for k in xrange(1, len(round_keys)):
+		dump_key = transpose(round_keys[k])
+		round_keys[k] = dump_key
+	print 'round_keys'
+	pprint(round_keys)
+	return round_keys
+			
+def put_leading_zeroes(d):
+	return '{:08d}'.format(d)
 
 # Makes a cyclic shift by n positions on the array l
 def rot_word(l):
 	return l[1:] + l[:1]
 
+def sub_word(C):
+	S = []
+	for c in C:
+		if c[0] == ' ': c[0] = '0'
+		if c[1] == ' ': c[1] = '0'
+		S.append(sbox[c[0]][c[1]])
+	return S
+	
+# Galois Multiplication
+def galoisMult(a, b):
+    p = 0
+    hiBitSet = 0
+    for i in range(8):
+        if b & 1 == 1:
+            p ^= a
+        hiBitSet = a & 0x80
+        a <<= 1
+        if hiBitSet == 0x80:
+            a ^= 0x1b
+        b >>= 1
+    return p % 256
+	
 def T(C):
 	return C
 	
-def xor(X, Y):
-	return X
+def xor(A, B):
+	result = []
+	for i, j in zip(A, B):
+		result.append(hex(int(i, 16) ^ int(j, 16))[2:].zfill(2))
+	return result
+		
 		
 	
 def build_state_matrix(block):
 	raw_state = list(chunks(block, 4))
-	state = [[0, 0, 0, 0], 
-			 [0, 0, 0, 0], 
-			 [0, 0, 0, 0], 
-			 [0, 0, 0, 0]]
-	# Calculate transpose matrix
-	for i in range(len(state)):
-		for j in range(len(state[0])):
-			state[j][i] = raw_state[i][j]
+	state = transpose(raw_state)
 	print 'state:'
 	pprint(state)
 	return state
 	
 def build_key_matrix(cipher_key):
 	raw_key = list(chunks(cipher_key, 4))
-	key = [[0, 0, 0, 0], 
-		   [0, 0, 0, 0], 
-		   [0, 0, 0, 0], 
-		   [0, 0, 0, 0]]
-	# Calculate transpose matrix
-	for i in range(len(key)):
-		for j in range(len(key[0])):
-			key[j][i] = raw_key[i][j]
+	key = transpose(raw_key)
 	print 'key:'
 	pprint(key)
 	return key
+	
+def transpose(M):
+	return [list(i) for i in zip(*M)]
+	
 
 # Yield successive n-sized chunks from l.
 def chunks(l, n):
@@ -132,7 +190,6 @@ def process_plaintext(plaintext):
 	for j in xrange(len(blocks)):
 		print_list('blocks['+str(j)+']', blocks[j], 2)
 	return blocks
-	
 
 # Print lists with a space every j elements with a message as prefix.
 def print_list(msg, l, j):
